f2e885a8a6b0500a3a7dbd2b07e5071689ea878b
sumit <krsumit.skr@gmail.com>
2023-07-11 17:20:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/movieproapp/Services/MovieDataServices.java
@@ -0,0 +1,22 @@
+package com.example.movieproapp.Services;
+
+import com.example.movieproapp.Model.Result;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface MovieDataServices {
+
+    //BaseURl
+    //https://api.themoviedb.org/3
+
+    //End Point URl
+    //movie/popular/157336?api_key=28c30bb5d9ea8862963e923ef74fae64
+
+    //API Key
+//    28c30bb5d9ea8862963e923ef74fae64
+
+    @GET("movie/popular")
+    Call<Result> getPopularMovie(@Query("api_key") String api_key);
+}
---
+++ app/src/main/java/com/example/movieproapp/Services/RetrofitInstance.java
@@ -0,0 +1,24 @@
+package com.example.movieproapp.Services;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+
+
+    public static Retrofit retrofit = null;
+    public static String BASE_URL = "https://api.themoviedb.org/3";
+
+    public static MovieDataServices getService(){
+
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+        }
+        return retrofit.create(MovieDataServices.class);
+    }
+
+}
